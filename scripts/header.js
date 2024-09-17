d75d33e98280af020e19f713080a6d44f516fe01
The Deadlock Protocol <thio@deadlock-protocol.com>
2024-09-17 15:35:32 +0700

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://cdn.jsdelivr.net/gh/The-Deadlock-Protocol/CDN/content/header.json')
+    fetch('https://cdn.jsdelivr.net/gh/The-Deadlock-Protocol/CDN@latest/content/header.json')
         .then(response => response.json())
         .then(headerData => {
             const header = document.querySelector('header');
